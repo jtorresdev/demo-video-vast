232aab0717d6530fccdf99f149b5e874841ab7e2
Jesus Torres <jesusk01@gmail.com>
2019-03-26 17:50:54 -0500

    mobile preview shit

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -1,99 +1,44 @@
 /*Only needed for the controls*/
-  phone = document.getElementById("phone_1"),
-  iframe = document.getElementById("frame_1");
-
+(phone = document.getElementById('phone_1')), (iframe = document.getElementById('frame_1'));
 
 /*View*/
 function updateView(view) {
-  if (view) {
-    phone.className = "phone view_" + view;
-  }
+	if (view) {
+		phone.className = 'phone view_' + view;
+	}
 }
 
 /*Controls*/
 function updateIframe() {
+	// preload iphone width and height
+	phone.style.width = '375px';
+	phone.style.height = '667px';
 
-  // preload iphone width and height
-  phone.style.width = "375px";
-  phone.style.height = "667px";
-
-  /*Idea by /u/aerosole*/
-  document.getElementById("wrapper").style.perspective = (
-    document.getElementById("iframePerspective").checked ? "1300px" : "none"
-  );
-
+	/*Idea by /u/aerosole*/
+	document.getElementById('wrapper').style.perspective = 'none';
 }
 
 updateIframe();
 
-/*Events*/
-document.getElementById("controls").addEventListener("change", function() {
-  updateIframe();
-});
+iframe = document.getElementById('frame_1');
 
-document.getElementById("views").addEventListener("click", function(evt) {
-  updateView(evt.target.value);
-});
+if (iframe.attachEvent) {
+	iframe.attachEvent('onload', function() {
+		afterLoading();
+	});
+} else {
+	iframe.onload = function() {
+		afterLoading();
+	};
+}
 
-document.getElementById("phones").addEventListener("click", function(evt) {
-
-  if(evt.target.value == 1){
-    // iphone 6
-    width = 375;
-    height = 667; 
-  }
-
-  if(evt.target.value == 2){
-    // samsung
-    width = 400;
-    height = 640; 
-  }
-
-  if(evt.target.value == 3){
-    // microsoft
-    width = 320;
-    height = 480;  
-  }
-
-  if(evt.target.value == 4){
-    // htc
-    width = 360;
-    height = 640; 
-  }
-
-  if(evt.target.value == 5){
-    // ipad mini
-    width = 768;
-    height = 1024; 
-  }
-
-    phone.style.width = width + "px";
-    phone.style.height = height + "px"; 
-
-});
-
-
- iframe = document.getElementById('frame_1');
-
-  if (iframe.attachEvent){
-      iframe.attachEvent("onload", function(){
-          afterLoading();
-      });
-  } else {
-      iframe.onload = function(){
-          afterLoading();
-      };
-  }
-
-function afterLoading(){
-
-    setTimeout(function() {
-        phone.className = "phone view_2";
-        setTimeout(function() {
-            // do second thing
-            phone.className = "phone view_2 rotate";
-            document.getElementById('loader').style.display = 'none';
-        }, 1000);
-    }, 1000);
-
+function afterLoading() {
+	setTimeout(function() {
+		phone.className = 'phone view_2';
+		setTimeout(function() {
+			// do second thing
+			phone.className = 'phone view_2 rotate';
+			document.getElementById('loader').style.display = 'none';
+		}, 1000);
+	}, 1000);
 }
