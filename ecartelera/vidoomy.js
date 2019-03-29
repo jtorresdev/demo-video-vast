3ad662abe5c160b702a6e8a491d16c3c7e84c45b
Jesus Torres <jesusk01@gmail.com>
2019-03-29 16:20:53 -0500

    js

--- ecartelera/vidoomy.js
+++ ecartelera/vidoomy.js
@@ -1,5 +1,10 @@
+var selectorType = 'class'
+var idOrclass = 'article'
+var lineBreak = 4
 var iframe = document.createElement('iframe')
 iframe.width = '720px'
 iframe.height = '550px'
 iframe.src = '../desktop770x550.html'
-document.getElementById('player-ad').appendChild(iframe)
+var element = selectorType == 'class' ? document.getElementsByClassName('article')[0] : document.getElementsById('article')
+var nodeToInsertBefore = element.querySelectorAll('p')[lineBreak]
+nodeToInsertBefore.parentNode.insertBefore(iframe, nodeToInsertBefore)
