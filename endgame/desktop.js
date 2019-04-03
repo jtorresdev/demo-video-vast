a2446daee5dc757f8e13a749904390243e1d6049
Jesus Torres <jesusk01@gmail.com>
2019-04-03 10:50:50 -0500

    base url

--- endgame/desktop.js
+++ endgame/desktop.js
@@ -19,7 +19,7 @@ var newHeight = parseInt(newWidth.replace('px', '')) * 0.5625 + 'px';
 var marginTop = '60px';
 var marginLeft = '10px';
 
-var base_url = "http://localwebapp/video/endgame"
+var base_url = "https://jtorresdev.github.io/demo-video-vast/endgame"
 
 
 var hidePlayerButtons = function() {
